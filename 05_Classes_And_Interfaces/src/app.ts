e23ffba440f82bcc05dd1ff6d22fc5d1d74a1f21
AbhishekChamp <abhishek96.r@gmail.com>
2022-09-05 23:37:23 +0530

    Section-5 Complete

--- 05_Classes_And_Interfaces/src/app.ts
+++ 05_Classes_And_Interfaces/src/app.ts
@@ -10,7 +10,8 @@ add = (n1: number, n2: number) => {
 };
 
 interface Named {
-    readonly name: string;
+    readonly name?: string;
+    outputName?: string; // Optional parameter
 }
 
 interface Greetable extends Named {
@@ -18,22 +19,32 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-    name: string;
+    name?: string;
     age = 25;
 
-    constructor(n: string) {
-        this.name = n;
+    constructor(n?: string) {
+        if (n) {
+            this.name = n;
+        }
     }
 
     greet(phrase: string): void {
-        console.log(phrase + " " + this.name);
+        if (this.name) {
+            console.log(phrase + " " + this.name);
+        } else {
+            console.log("Hi!");
+        }
     }
 }
 
 let user1: Greetable;
+let user2: Greetable;
 
 user1 = new Person("Abhishek");
 
+user2 = new Person();
+
 user1.greet("Hi There - I am");
+user2.greet("Hi There - I am");
 
 console.log(add(8, 9));
