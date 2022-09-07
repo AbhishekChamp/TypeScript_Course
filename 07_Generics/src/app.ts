5ee99504f0cd2db99d4a2de8ee8d9135b13e9030
AbhishekChamp <abhishek96.r@gmail.com>
2022-09-07 23:56:40 +0530

    Section-7 Part-1

---
+++ 07_Generics/src/app.ts
@@ -0,0 +1,8 @@
+// const names: Array<string> = []; // string[]
+// // name[0].split(' ');
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("This is done!");
+//     }, 2000);
+// });
